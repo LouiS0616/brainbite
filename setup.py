f5f4458971f243437f3ca8e6bd08ddabe3025bdd
Loui Sakaki <e1352207@outlook.jp>
2018-08-06 20:58:10 +0900

    add some meta files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brainbite',
+    version='0.1.0',
+    description='A python bit my brain.',
+    url='https://github.com/LouiS0616/brainbite',
+    license='MIT',
+    author='LouiSakaki',
+    author_email='',
+    packages=find_packages()
+)
