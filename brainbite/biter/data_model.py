c98a676f9fa4a7b32418d305df5a7188b20fc333
Loui Sakaki <e1352207@outlook.jp>
2018-08-21 14:49:41 +0900

    Fix typos.

--- brainbite/biter/data_model.py
+++ brainbite/biter/data_model.py
@@ -27,35 +27,35 @@ class BFDataModel:
         self._data[self._index] = value
 
     def increment_pointer(self):
-        """This correspond to operation '>'."""
+        """This corresponds to operation '>'."""
         self.pointer += 1
 
     def decrement_pointer(self):
-        """This correspond to operation '<'."""
+        """This corresponds to operation '<'."""
         self.pointer -= 1
 
     def increment_pointee(self):
-        """This correspond to operation '+'."""
+        """This corresponds to operation '+'."""
         self.pointee += 1
 
     def decrement_pointee(self):
-        """This correspond to operation '-'."""
+        """This corresponds to operation '-'."""
         self.pointee -= 1
 
     def conditional_skip(self):
-        """This correspond to operation '['."""
+        """This corresponds to operation '['."""
         return self.pointee == 0
 
     def unconditional_jump(self):
-        """This correspond to operation ']'."""
+        """This corresponds to operation ']'."""
         pass
 
     def print_pointee(self):
-        """This correspond to operation '.'."""
+        """This corresponds to operation '.'."""
         sys.stdout.write(
             chr(self.pointee)
         )
 
     def input_to_pointee(self):
-        """This correspond to operation ','."""
+        """This corresponds to operation ','."""
         self.pointee = ord(sys.stdin.read(1))
